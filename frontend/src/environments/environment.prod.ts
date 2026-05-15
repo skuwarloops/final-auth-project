02ee54b8ead24656284138b1c48f85816aa78625
skuwarloops <sabanclarc2@gmail.com>
2026-05-15 13:13:05 +0800

    fix: remove vercel files switch to render

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend-url.onrender.com/api'  // Update after deployment
+  apiUrl: 'https://final-auth-project.onrender.com'
 };
